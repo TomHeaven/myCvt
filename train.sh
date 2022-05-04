0a8d5f8811769f4f5fe97cf0b194578bc679ac43
TomHeaven <TomHeaven@qq.com>
2022-05-04 10:55:21 +0800

    add pretrained weights for cvt

--- train.sh
+++ train.sh
@@ -1,1 +1,4 @@
-nohup bash run.sh -g 4 -t train --cfg experiments/cifar100/cvt/local-cvt-13-224x224.yaml > train_cifar100_local_cvt.log &
+#at now + 8 hours -f ~/myscript.sh
+
+nohup bash -c "sleep $(echo '2 * 60 * 60' | bc) ; bash run.sh -g 4 -t train --cfg experiments/cifar100/cvt/cvt-13-224x224.yaml" > train_cifar100_cvt.log &
+#nohup bash run.sh -g 4 -t train --cfg experiments/cifar100/cvt/local-cvt-13-224x224.yaml > train_cifar100_local_cvt.log &
