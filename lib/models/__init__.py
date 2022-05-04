5966b4f6475d968c5e759c756b96f47638222d3d
TomHeaven <TomHeaven@qq.com>
2022-05-04 17:57:25 +0800

    add local-cvt2

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import imp
 
 from .cls_cvt import *
 from .cls_local_cvt import LocalConvolutionalVisionTransformer
+from .cls_local_cvt2 import LocalConvolutionalVisionTransformer2
 
 from .registry import *
 
